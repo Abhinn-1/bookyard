c4db667865788edfcd02938269204ced4185e4a2
Abhinn-1 <137832176+Abhinn-1@users.noreply.github.com>
2025-07-22 22:02:36 +0530

    Update Spinner.jsx

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Spinner = () => {
   return (
-    <div className='fixed inset-0 bg-black bg-opacity-20 flex flex-col items-center justify-center z-50'>
+    <div className='fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-30 flex flex-col items-center justify-center z-50'>
       <div className='w-16 h-16 border-4 border-t-transparent border-sky-600 rounded-full animate-spin'></div>
-      <p className='mt-4 text-lg text-gray-800 font-semibold text-center px-4'>
-        Please be patient — server might take upto 50 seconds to wake up
+      <p className='mt-4 text-lg text-white font-semibold text-center px-4'>
+        Please be patient — server might take up to 50 seconds to wake up
       </p>
     </div>
   );
